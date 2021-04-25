da6475f37968d152a07ec29c0088f50c0e5a2ecc
T-Aruga <baideneo@gmail.com>
2021-04-25 13:52:09 +0900

    Add auth function

---
+++ src/features/core/Core.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Auth from "../auth/Auth";
+
+// import styles from "./Core.module.css";
+
+
+const Core: React.FC = () => {
+
+  return (
+    <div>
+      <Auth />
+    </div>
+  );
+};
+
+export default Core;
